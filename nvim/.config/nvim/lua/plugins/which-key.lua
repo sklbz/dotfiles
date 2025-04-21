71c145fb0b1ddd42749ae996cae62a10cc8733ee
sklbz <sokol.bozanic@tumo.world>
2025-04-21 10:33:22 +0200

    updated nvim config

--- nvim/.config/nvim/lua/plugins/which-key.lua
+++ nvim/.config/nvim/lua/plugins/which-key.lua
@@ -6,4 +6,12 @@ return {
 		vim.o.timeoutlen = 100
 	end,
 	opts = {},
+	config = function()
+		local wk = require("which-key")
+		wk.add({
+			{ "<leader>d", group = "debug", mode = "n" },
+			{ "<leader>p", group = "pane", mode = "n" },
+			{ "<leader>s", group = "search", mode = "n" },
+		})
+	end,
 }
