60d66c9cb64659edd5735b1bab73ffc3594b46fe
sklbz <sokol.bozanic@tumo.world>
2025-07-28 12:16:05 +0200

    added mini ai

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,15 +1,16 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local config = require("nvim-treesitter.configs")
-      config.setup({
-				ensure_installed = {"lua", "rust", "vim"},
-        auto_install = true,
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end
-  }
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local config = require("nvim-treesitter.configs")
+			config.setup({
+				ensure_installed = { "lua", "rust", "vim" },
+				auto_install = true,
+				highlight = { enable = true },
+				indent = { enable = true },
+				textobjects = { enable = false },
+			})
+		end,
+	},
 }
