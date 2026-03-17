d57da96e8a7e19c6c56a825e89a9e3337f1a2b78
sklbz <sokol.bozanic@tumo.world>
2026-03-17 18:00:01 +0000

    fixed network nix config

--- nix/etc/nixos/modules/network.nix
+++ nix/etc/nixos/modules/network.nix
@@ -5,7 +5,8 @@
 	networking.nameservers = [ "8.8.8.8" "1.1.1.1" ];
 
 	environment.etc."resolv.conf".text = ''
-		nameserver 8.8.8.8
-		nameserver 1.1.1.1
+nameserver 127.0.0.53
+nameserver 8.8.8.8
+nameserver 1.1.1.1
 	'';
 }
